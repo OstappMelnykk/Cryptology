7a21be324ac0581c92f06b471f84378210f55fac
OstappMelnykk <79ostap@ukr.net>
2024-03-07 11:34:15 +0200

    commit2

---
+++ encryption/encryption/Controllers/CaesarController.cs
@@ -0,0 +1,210 @@
+﻿using CryptologyLabs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace encryption.Controllers
+{
+	public class CaesarController : Controller
+	{
+		private static Dictionary<char, int> frequencyTable;
+
+
+		public IActionResult Index(string content = "")
+		{
+			ViewBag.Content = content;
+			return View();
+
+		}
+
+
+		public IActionResult DownloadFile(string sourceText)
+		{
+			string fileName = "example_file.txt";
+			string filePath = Path.Combine("..", Directory.GetCurrentDirectory(), fileName);
+			System.IO.File.WriteAllText(filePath, sourceText);
+
+			return PhysicalFile(filePath, "application/octet-stream", fileName);
+		}
+
+
+
+
+		public IActionResult Encrypt(string sourceText, int step)
+		{
+			frequencyTable = new();
+
+			ViewBag.ToReturn = new string(sourceText.Select(v => EncodeChar(v, step)).ToArray());
+			ViewBag.sourceText = sourceText;
+
+			return View("Encrypt");
+		}
+
+		public IActionResult Decrypt(string sourceText, int step)
+		{
+			ViewBag.ToReturn = new string(sourceText.Select(v => DecodeChar(v, step)).ToArray());
+			ViewBag.sourceText = sourceText;
+
+			return View("Decrypt");
+
+		}
+
+		public IActionResult PrintFrequencyTable(string sourceText, int step)
+		{
+			ViewBag.Encounters = frequencyTable;
+			return View("PrintFrequencyTable");
+
+		}
+
+
+		public IActionResult Attack(string sourceText, string encryptedText)
+		{
+			int step = 0;
+
+			while (step < 10000)
+			{
+				if (new string(encryptedText.Select(v => DecodeChar(v, step)).ToArray()) == sourceText)
+				{
+					return Content(step.ToString());
+				}
+
+				step++;
+			}
+			if (step == 1000)
+				return Content("The brute force attack failed");
+
+
+			return null;
+		}
+
+
+
+
+
+
+
+		private char EncodeChar(char from, int step)
+		{
+			if (Alphabets.ukrainian.Contains(from))
+			{
+				var current = Alphabets.ukrainian.IndexOf(from);
+
+				if (frequencyTable.ContainsKey(Alphabets.ukrainianCapital[current]))
+					frequencyTable[Alphabets.ukrainianCapital[current]]++;
+				else
+					frequencyTable[Alphabets.ukrainianCapital[current]] = 1;
+
+				int index = (current + step) % Alphabets.ukrainianLen;
+
+				if (index < 0)
+					index += Alphabets.ukrainianLen;
+
+				return Alphabets.ukrainian[index];
+			}
+
+			if (Alphabets.ukrainianCapital.Contains(from))
+			{
+				var current = Alphabets.ukrainianCapital.IndexOf(from);
+
+				if (frequencyTable.ContainsKey(from))
+					frequencyTable[from]++;
+				else
+					frequencyTable[from] = 1;
+
+				int index = (current + step) % Alphabets.ukrainianLen;
+
+				if (index < 0)
+					index += Alphabets.ukrainianLen;
+
+				return Alphabets.ukrainianCapital[index];
+			}
+
+			if (from > 64 && from < 91)
+			{
+				int current = from - 'A';
+
+				if (frequencyTable.ContainsKey(from))
+					frequencyTable[from]++;
+				else
+					frequencyTable[from] = 1;
+
+				int index = (current + step) % Alphabets.englishLen;
+
+				if (index < 0)
+					index += Alphabets.englishLen;
+
+				return (char)(index + 'A');
+			}
+
+			if (from > 96 && from < 123)
+			{
+				int current = from - 'a';
+
+				if (frequencyTable.ContainsKey((char)(current + 'A')))
+					frequencyTable[(char)(current + 'A')]++;
+				else
+					frequencyTable[(char)(current + 'A')] = 1;
+
+				int index = (current + step) % Alphabets.englishLen;
+
+				if (index < 0)
+					index += Alphabets.englishLen;
+
+				return (char)(index + 'a');
+			}
+
+			return from;
+		}
+
+		private char DecodeChar(char from, int step)
+		{
+			if (Alphabets.ukrainian.Contains(from))
+			{
+				var current = Alphabets.ukrainian.IndexOf(from);
+
+				int index = (current - step) % Alphabets.ukrainianLen;
+
+				if (index < 0)
+					index += Alphabets.ukrainianLen;
+
+				return Alphabets.ukrainian[index];
+			}
+
+			if (Alphabets.ukrainianCapital.Contains(from))
+			{
+				var current = Alphabets.ukrainianCapital.IndexOf(from);
+
+				int index = (current - step) % Alphabets.ukrainianLen;
+
+				if (index < 0)
+					index += Alphabets.ukrainianLen;
+
+				return Alphabets.ukrainianCapital[index];
+			}
+
+			if (from > 64 && from < 91)
+			{
+				int current = from - 'A';
+
+				int index = (current - step) % Alphabets.englishLen;
+
+				if (index < 0)
+					index += Alphabets.englishLen;
+
+				return (char)(index + 'A');
+			}
+
+			if (from > 96 && from < 123)
+			{
+				int current = from - 'a';
+
+				int index = (current - step) % Alphabets.englishLen;
+
+				if (index < 0)
+					index += Alphabets.englishLen;
+
+				return (char)(index + 'a');
+			}
+
+			return from;
+		}
+	}
+}
---
+++ encryption/encryption/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace encryption.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> UploadFile(IFormFile file)
+		{
+			try
+			{
+				if (file != null && file.Length > 0)
+				{
+					var uploadsFolder = Path.Combine("..", Directory.GetCurrentDirectory(), "Files");
+
+					if (!Directory.Exists(uploadsFolder))
+					{
+						Directory.CreateDirectory(uploadsFolder);
+					}
+
+					var uniqueFileName = file.FileName;
+					var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+					using (var fileStream = new FileStream(filePath, FileMode.Create))
+					{
+						await file.CopyToAsync(fileStream);
+					}
+
+					return RedirectToAction("Index", "Caesar", new { content = ReadTextFile(filePath) });
+				}
+				else return NotFound("Error. There is no uploaded file");
+
+			}
+			catch (Exception ex)
+			{
+				return BadRequest($"An error occurred: {ex.Message}");
+			}
+		}
+
+
+
+		private string ReadTextFile(string filePath)
+		{
+			if (System.IO.File.Exists(filePath) && Path.GetExtension(filePath)?.ToLower() == ".txt")
+			{
+				var content = System.IO.File.ReadAllText(filePath);
+				return content;
+			}
+			else
+			{
+				throw new ArgumentException("Invalid file path or file type.");
+			}
+
+		}
+	}
+}
